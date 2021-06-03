5a4f559ce7174a88f05e5e83a2f23f180bbdc70e
FaBLee <58670304+fablee1@users.noreply.github.com>
2021-06-03 20:04:51 +0100

    Added authorization and polished ui

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
+const loginModal = document.querySelector('#loginWrapper')
+const loginMessage = document.querySelector('#loginMessage')
+const outputList = document.querySelector('#outputs ol')
+
 const eraseEverything = async () => {
-  let username = "mikelitoris34@icloud.com";
-  let password = "bollocks69";
-  let response = await fetch(
-    `https://striveschool-api.herokuapp.com/api/account/login?username=${username}&password=${password}`,
-    {
-      method: "POST",
-    }
-  );
-  let key = await response.json();
-  const apiKey = `Bearer ${key.access_token}`;
-  //   console.log(apiKey);
+
+  const authToken = "Bearer " + localStorage.getItem('token')
 
   const array = await fetch(
     `https://striveschool-api.herokuapp.com/api/comments/`,
     {
       method: "GET",
       headers: {
-        Authorization: apiKey,
+        "Authorization": authToken,
         "Content-type": "application/json",
       },
     }
@@ -27,21 +22,50 @@ const eraseEverything = async () => {
         console.log(`There's no comments to erase`);
       } else {
         console.log(`Initiating wiping of the database`);
+        outputList.innerHTML = ''
         for (let i = 0; i < data.length; i++) {
           await fetch(
             `https://striveschool-api.herokuapp.com/api/comments/${data[i]._id}`,
             {
               method: "DELETE",
               headers: {
-                Authorization: apiKey,
+                "Authorization": authToken,
               },
             }
           );
-          console.log(
-            `Operation №${i}: The comment with ID: ${data[i]._id} has been deleted`
-          );
+          const li = document.createElement('li')
+          li.innerText = `Operation №${i}: The comment with ID: ${data[i]._id} has been deleted`
+          outputList.appendChild(li)
         }
         console.log(`There's no comments left`);
       }
     });
 };
+
+const login = async (e) => {
+  e.preventDefault()
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value
+
+  const response = await fetch(`https://striveschool-api.herokuapp.com/api/account/login?username=${email}&password=${password}`, { method: "POST" })
+  if(await response.ok) {
+    const data = await response.json()
+    localStorage.setItem('token', data.access_token)
+
+    loginMessage.innerText = 'Succesfully logged in!'
+    loginMessage.style.color = 'green'
+    setTimeout(() => loginModal.style.display = 'none', 2000)
+  } else {
+    loginMessage.innerText = 'Invalid Email/Password, try again.'
+    loginMessage.style.color = 'red'
+  }
+
+}
+
+window.onload = () => {
+  if(!localStorage.getItem('token')) {
+    loginModal.style.display = 'block'
+  }
+
+  document.getElementsByTagName('form')[0].addEventListener('submit', (e) => login(e))
+}
